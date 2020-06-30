7579f6e3eb2900a72dd860012aa2a6174d4ee68a
pkp09 <pandeyprashant@live.in>
2020-06-30 11:43:41 +0530

    ZUUL Service added

---
+++ src/test/java/com/prashant/microservices/neflixzuulapigatewayserver/NeflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.microservices.neflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
